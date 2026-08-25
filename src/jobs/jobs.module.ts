05fd917e7fcfd7ef5b5af164836c5fd231f8ee11
Bouynaty <jojojiho07@gmail.com>
2026-08-25 19:01:34 +0100

    security: Add cursor-based, resumable Stellar payment synchronization (#3)

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,14 +1,18 @@
-import { Module } from "@nestjs/common";
+import { Module } from "@nestj/common";
 import { ContractAnchoringService } from "../proofs/contract-anchoring.service";
 import { AnchoringReconcilerService } from "./anchoring-reconciler.service";
 import { AnchoringWorkerService } from "./anchoring-worker.service";
+import { PaymentSyncService } from "../payments/payment-sync.service";
+import { PaymentSyncWorkerService } from "./payment-sync-worker.service";
 
 @Module({
   providers: [
     ContractAnchoringService,
     AnchoringWorkerService,
-    AnchoringReconcilerService,
+    Anchoring ReconcilerService,
+    PaymentSyncService,
+    PaymentSyncWorkerService,
   ],
-  exports: [AnchoringWorkerService, AnchoringReconcilerService],
+  exports: [AnchoringWorkerService, AnchoringReconcilerService, PaymentSyncWorkerService],
 })
 export class JobsModule {}
